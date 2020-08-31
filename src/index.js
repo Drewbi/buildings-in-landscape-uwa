acde9a97c8ede6b22ec46c86bae7e64a89748e92
Drewbi <drewalexander986@gmail.com>
2020-08-31 21:07:47 +0800

    Add babel

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { Viewer } from "panolens";
 import { initAllPano } from "./util/pano";
 import "./main.css";
 
-const root = document.getElementById("root");
+const root = document.getElementById('root')
 const viewer = new Viewer({ container: root })
 initAllPano(viewer);
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
   ],
   module: {
     rules: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
