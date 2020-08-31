afe6fd4949ec9008ff1c25083f6ceb11487a32e6
Drewbi <drewalexander986@gmail.com>
2020-08-31 21:11:47 +0800

    Alter prettier config

--- src/util/pano.js
+++ src/util/pano.js
@@ -1,22 +1,22 @@
-import { ImagePanorama } from "panolens";
-import { state, getLocationById } from "./state";
+import { ImagePanorama } from 'panolens'
+import { state, getLocationById } from './state'
 
 const initPano = (viewer, location) => {
-  const panorama = new ImagePanorama(location.src);
-  viewer.add(panorama);
-  location.panorama = panorama;
-  return panorama;
-};
+  const panorama = new ImagePanorama(location.src)
+  viewer.add(panorama)
+  location.panorama = panorama
+  return panorama
+}
 
 const setPano = (viewer, id) => {
-  const location = getLocationById(id);
-  console.log(location);
-  viewer.setPanorama(location.panorama);
-};
+  const location = getLocationById(id)
+  console.log(location)
+  viewer.setPanorama(location.panorama)
+}
 
 const initAllPano = (viewer) => {
-  state.forEach((location) => initPano(viewer, location));
-  setPano(viewer, 1);
-};
+  state.forEach((location) => initPano(viewer, location))
+  setPano(viewer, 1)
+}
 
-export { initAllPano };
+export { initAllPano }
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist'
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[hash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[hash].js'
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Buildings in Landscape",
-      template: "src/index.html",
-    }),
+      title: 'Buildings in Landscape',
+      template: 'src/index.html'
+    })
   ],
   module: {
     rules: [
-      { 
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
-    ],
-  },
-};
+        use: ['file-loader']
+      }
+    ]
+  }
+}
