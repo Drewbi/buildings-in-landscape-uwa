1279287d07ea7dea52c04f19b8f511ec4d07b1b6
Drewbi <drewalexander986@gmail.com>
2020-08-31 20:16:49 +0800

    Add precommit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { initAllPano } from "./util/pano";
 import "./main.css";
 
 const root = document.getElementById("root");
-const viewer = new Viewer({ container: root });
+const viewer = new Viewer({ container: root })
 initAllPano(viewer);
