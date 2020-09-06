88eba440c6d5ab8bb497360e73f7bd924ded9dc1
21964392 <69237163+21964392@users.noreply.github.com>
2020-09-07 00:41:30 +0800

    Added Nav Buttons to Bottom of Viewer
    -Added HTML img for Back and Forward
    -Added HTML loader for webpack
    -Nav code is in Index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { Viewer } from 'panolens'
 import { initAllPano } from './util/pano'
 import './main.css'
+import html from './index.html'
+import { getLocationById } from './util/state'
+
+let currID = 1
 
 const root = document.getElementById('root')
 const viewer = new Viewer({
@@ -9,4 +13,36 @@ const viewer = new Viewer({
   cameraFov: 85,
   output: 'overlay'
 })
+const backbutton = document.images['jsback']
+const forwardbutton = document.images['jsforward']
+
+backbutton.onclick = function() {imgBack()}
+forwardbutton.onclick = function() {imgForward()}
+
+function imgBack(){
+	console.log(currID)
+	const id = getPrev(currID)
+	const location = getLocationById(id)
+	viewer.setPanorama(location.panorama)
+	return true;
+}
+
+function imgForward(){
+	console.log(currID)
+	const id = getNext(currID)
+	const location = getLocationById(id)
+	viewer.setPanorama(location.panorama)
+	return true;
+}
+
+function getNext(iid){
+	currID = getLocationById(iid).navMarkers[0].to
+	return currID
+}
+
+function getPrev(iid){
+	currID = getLocationById(iid).navMarkers[1].to
+	return currID
+}
+
 initAllPano(viewer)
--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,13 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
-      }
+      },
+	  {
+        test: /\.html$/i,
+        loader: 'html-loader',
+		options: {
+        },
+      },
     ]
   }
 }
