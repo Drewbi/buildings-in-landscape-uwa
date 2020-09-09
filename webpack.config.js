9f876f628cb21a4ffc9555938bdcecfcad02ddb3
Drewbi <drewalexander986@gmail.com>
2020-09-09 15:03:24 +0800

    Revert "Added Nav Buttons to Bottom of Viewer"
    This reverts commit 88eba440c6d5ab8bb497360e73f7bd924ded9dc1.

--- webpack.config.js
+++ webpack.config.js
@@ -33,13 +33,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
-      },
-	  {
-        test: /\.html$/i,
-        loader: 'html-loader',
-		options: {
-        },
-      },
+      }
     ]
   }
 }
