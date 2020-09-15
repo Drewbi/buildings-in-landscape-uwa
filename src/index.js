1866ef9bc76072cbce26b59aea22bb09727182e2
Drewbi <drewalexander986@gmail.com>
2020-09-16 02:15:48 +0800

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import { Viewer } from 'panolens'
 import { initAllPano } from './util/pano'
 import { initSidebar } from './util/info'
 import './main.css'
-import './assets/icons/close.svg'
-import './index.html'
 
 const root = document.getElementById('root')
 const viewer = new Viewer({
