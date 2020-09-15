81bd0614fc41b1121441a99de402b95dd0c5ba4f
Drewbi <drewalexander986@gmail.com>
2020-09-16 02:10:01 +0800

    Fix sidebar data flow

---
+++ src/util/info.js
@@ -0,0 +1,60 @@
+import { Infospot, DataImage } from 'panolens'
+import infoMarkers from '../data/info.json'
+
+const initSidebar = () => {
+  const close = document.getElementById('poiClose')
+  close.addEventListener('click', () => {
+    setSidebarOpen(false)
+  })
+}
+
+const loadInfoMarkers = (location) => {
+  if (location.infoMarkers) {
+    location.infoMarkers.forEach((marker) => {
+      const markerInfo = getInfoMarkerById(marker.markerId)
+
+      const infoSpot = new Infospot(300, DataImage.Info)
+      const { x, y, z } = marker.position
+      infoSpot.position.set(x, y, z)
+
+      infoSpot.addHoverText(markerInfo.title)
+      infoSpot.userData = markerInfo
+
+      infoSpot.addEventListener('click', ({ target }) => {
+        setSidebarContent(target.userData)
+        setSidebarOpen(true)
+        // target.focus()
+        target.onDismiss()
+      })
+
+      location.panorama.add(infoSpot)
+    })
+  }
+}
+
+const setSidebarOpen = (open) => {
+  const infoPane = document.getElementById('infoPane')
+  open
+    ? infoPane.classList.remove('hidePane')
+    : infoPane.classList.add('hidePane')
+}
+
+const setSidebarContent = (info) => {
+  const titleElement = document.getElementById('poiTitle')
+  titleElement.innerHTML = info.title
+  const bodyElement = document.getElementById('poiText')
+  bodyElement.innerHTML = info.text
+}
+
+const getInfoMarkerById = (id) => {
+  return infoMarkers.find((entry) => entry.id === id)
+}
+
+export {
+  infoMarkers,
+  initSidebar,
+  loadInfoMarkers,
+  setSidebarOpen,
+  setSidebarContent,
+  getInfoMarkerById
+}
