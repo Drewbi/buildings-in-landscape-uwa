78972363a475b2d864783416cd4af6c1883ffd8c
Drewbi <drewalexander986@gmail.com>
2020-09-16 02:08:40 +0800

    Add async support for image loading

--- src/util/pano.js
+++ src/util/pano.js
@@ -1,13 +1,15 @@
-import { ImagePanorama, Infospot, DataImage } from 'panolens'
+import { ImagePanorama } from 'panolens'
 import { Vector3 } from 'three'
-import { state, getLocationById } from './state'
-import infoMarkers from '../data/info.json'
+import { locations, getLocationById } from './location'
+import { loadInfoMarkers, setSidebarOpen } from './info'
 
-const initPano = (viewer, location) => {
-  const panorama = new ImagePanorama(location.src)
+const initPanorama = async (viewer, location) => {
+  const { default: image } = await import('../assets/images/' + location.src)
+  location.image = image
+  const panorama = new ImagePanorama(image)
   viewer.add(panorama)
   location.panorama = panorama
-  return panorama
+  panorama.addEventListener('load', () => setSidebarOpen(false))
 }
 
 const initNavMarkers = (location) => {
@@ -18,28 +20,18 @@ const initNavMarkers = (location) => {
   })
 }
 
-
-
-const loadInfoMarkers = (location) => {
-  location.infoMarkers.forEach((marker) => {
-    const infoSpot = new Infospot(300, DataImage.Info)
-    infoSpot.addHoverText(infoMarkers[0].title)
-    console.log(infoMarkers)
-    const { x, y, z } = marker.position
-    infoSpot.position.set(x, y, z)
-    location.panorama.add(infoSpot)
-  })
-}
-
 const setPano = (viewer, id) => {
   const location = getLocationById(id)
   viewer.setPanorama(location.panorama)
 }
 
-const initAllPano = (viewer) => {
-  state.forEach((location) => initPano(viewer, location))
-  state.forEach((location) => initNavMarkers(location))
-  state.forEach((location) => loadInfoMarkers(location))
+const initAllPano = async (viewer) => {
+  const panoPromises = locations.map((location) =>
+    initPanorama(viewer, location)
+  )
+  await Promise.all(panoPromises)
+  locations.forEach((location) => initNavMarkers(location))
+  locations.forEach((location) => loadInfoMarkers(location))
   setPano(viewer, 1)
 }
 
