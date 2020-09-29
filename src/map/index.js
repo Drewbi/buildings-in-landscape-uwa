e7b6ea59686dc8ef788733acd4af25568043875d
antoniojoboy <joboyantonio@gmail.com>
2020-09-29 20:43:28 +0800

    udpate map javascript

--- src/map/index.js
+++ src/map/index.js
@@ -1,7 +1,7 @@
 import mapboxgl from 'mapbox-gl'
 import './map.css'
 import { locations } from '../util/location'
-
+//imports
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW50b25pb2pvYm95IiwiYSI6ImNrZjFxb3huazIxdG8yc2w5MGR2NTJ2NmwifQ.jclnR2Zqjv8UUUM1r2w8fw'
 
@@ -17,10 +17,7 @@ const map = new mapboxgl.Map({
 const currentPos = document.createElement('div')
 currentPos.id = 'current-marker'
 const currentMarker = new mapboxgl.Marker(currentPos)
-  .setLngLat([
-    locations[0].positions.lon + 100,
-    locations[0].positions.lat + 100
-  ])
+  .setLngLat([locations[0].positions.lon, locations[0].positions.lat])
   .addTo(map)
 
 var nav = new mapboxgl.NavigationControl()
