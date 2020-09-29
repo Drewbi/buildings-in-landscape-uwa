98562db9ce9cccc5bf7b68e3d0fc52c597955da1
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2020-09-29 13:06:08 +0800

    update map javascript

--- src/map/index.js
+++ src/map/index.js
@@ -5,6 +5,9 @@ import { locations } from '../util/location'
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW50b25pb2pvYm95IiwiYSI6ImNrZjFxb3huazIxdG8yc2w5MGR2NTJ2NmwifQ.jclnR2Zqjv8UUUM1r2w8fw'
 
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
+
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
