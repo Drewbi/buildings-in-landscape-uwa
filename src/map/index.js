e3f49c47389c4aa5b78e487feaf0628040ebc727
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2020-09-29 13:45:24 +0800

    updata map

--- src/map/index.js
+++ src/map/index.js
@@ -5,9 +5,6 @@ import { locations } from '../util/location'
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW50b25pb2pvYm95IiwiYSI6ImNrZjFxb3huazIxdG8yc2w5MGR2NTJ2NmwifQ.jclnR2Zqjv8UUUM1r2w8fw'
 
-var nav = new mapboxgl.NavigationControl();
-map.addControl(nav, 'top-left');
-
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
@@ -20,8 +17,11 @@ const map = new mapboxgl.Map({
 const currentPos = document.createElement('div')
 currentPos.id = 'current-marker'
 const currentMarker = new mapboxgl.Marker(currentPos)
-  .setLngLat([locations[0].positions.lon, locations[0].positions.lat])
+  .setLngLat([locations[0].positions.lon+100, locations[0].positions.lat+100])
   .addTo(map)
+
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
 
 const initMapMarkers = () => {
   locations.forEach((entry) => {
