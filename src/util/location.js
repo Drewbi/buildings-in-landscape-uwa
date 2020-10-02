e85f7992a7cecfd1910071da3a6056db2e7cd89f
Drewbi <drewalexander986@gmail.com>
2020-10-02 17:07:10 +0800

    Refactor prefetch code

--- src/util/location.js
+++ src/util/location.js
@@ -8,35 +8,18 @@ const prefetchImages = (location) => {
   const prefetchElement = document.getElementById('prefetch')
   if (prefetchElement !== null) {
     prefetchElement.innerHTML = ''
-
-    // Nav markers
-    if (location.navMarkers) {
-      location.navMarkers.forEach((marker) => {
-        const nextLocation = getLocationById(marker.to)
-        const imageElement = document.createElement('img')
-        imageElement.setAttribute('src', nextLocation.image)
-        imageElement.setAttribute('style', 'display: none;')
-        prefetchElement.appendChild(imageElement)
-      })
-    }
-
-    // Back marker
-    if (location.backMarker) {
-      const backLocation = getLocationById(location.backMarker.to)
-      const backImage = document.createElement('img')
-      backImage.setAttribute('src', backLocation.image)
-      backImage.setAttribute('style', 'display: none;')
-      prefetchElement.appendChild(backImage)
-    }
-
-    // Forward marker
-    if (location.forwardMarker) {
-      const forwardLocation = getLocationById(location.forwardMarker.to)
-      const forwardImage = document.createElement('img')
-      forwardImage.setAttribute('src', forwardLocation.image)
-      forwardImage.setAttribute('style', 'display: none;')
-      prefetchElement.appendChild(forwardImage)
-    }
+    // We want to have the back and forward markers included in the navMarkers array
+    // If there are no navMarkers we need to create a new array so we can add to it
+    if (!location.navMarkers) location.navMarkers = []
+    if (location.forwardMarker) location.navMarkers.push(location.forwardMarker)
+    if (location.backMarker) location.navMarkers.push(location.backMarker)
+    location.navMarkers.forEach((marker) => {
+      const nextLocation = getLocationById(marker.to)
+      const imageElement = document.createElement('img')
+      imageElement.setAttribute('src', nextLocation.image)
+      imageElement.setAttribute('style', 'display: none;')
+      prefetchElement.appendChild(imageElement)
+    })
   }
 }
 
