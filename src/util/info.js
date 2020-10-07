7ec9d7f54596df3882484dc746a69a35fd00144d
Drewbi <drewalexander986@gmail.com>
2020-10-07 13:41:30 +0800

    Info fix

--- src/util/info.js
+++ src/util/info.js
@@ -55,14 +55,16 @@ const setSidebarContent = (info) => {
   const bodyElement = document.createElement('p')
   bodyElement.innerText = info.text
   infoPane.appendChild(bodyElement)
-  info.images.forEach(async (image) => {
-    const imageElement = document.createElement('img')
-    const { default: imageSrc } = await import(
-      '../assets/images/' + image.id + '.JPG'
-    )
-    imageElement.setAttribute('src', imageSrc)
-    infoPane.appendChild(imageElement)
-  })
+  if (info.images) {
+    info.images.forEach(async (image) => {
+      const imageElement = document.createElement('img')
+      const { default: imageSrc } = await import(
+        '../assets/images/' + image.id + '.JPG'
+      )
+      imageElement.setAttribute('src', imageSrc)
+      infoPane.appendChild(imageElement)
+    })
+  }
 }
 
 const getInfoMarkerById = (id) => {
