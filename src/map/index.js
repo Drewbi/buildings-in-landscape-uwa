b07c28e135f1af700900b5c0d967f65987e11b81
Drew Alexander <drewalexander986@gmail.com>
2024-10-12 19:12:09 +0800

    New mapbox token

--- src/map/index.js
+++ src/map/index.js
@@ -3,7 +3,7 @@ import './map.css'
 import { locations } from '../util/location'
 
 mapboxgl.accessToken =
-  'pk.eyJ1IjoiYW50b25pb2pvYm95IiwiYSI6ImNrZjFxb3huazIxdG8yc2w5MGR2NTJ2NmwifQ.jclnR2Zqjv8UUUM1r2w8fw'
+  'pk.eyJ1IjoiZHJ1YmkwIiwiYSI6ImNtMjYyM3E5cjB0OXcycnNhdXltMjlvZWgifQ.T-oOfLr-yx84Y23eOYi12g'
 
 const map = new mapboxgl.Map({
   container: 'map',
